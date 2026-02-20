ed7cc5d542f3fc7e560064d0a2aa748720d600a7
sunny sharma <sunny.w3c@gmail.com>
2026-02-21 00:59:06 +0530

    update files

---
+++ public/build/assets/js/app-nQUnFIoG.js
@@ -0,0 +1,1 @@
+import{a as e}from"./axios-Cfc3znND.js";import{m as o}from"./alpine-BDrIrP2t.js";window.axios=e;window.axios.defaults.headers.common["X-Requested-With"]="XMLHttpRequest";window.Alpine=o;o.start();
